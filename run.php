a390d8d38a418f1e925e60c691f3dc32833c43fd
Levi <levi@teklife.info>
2016-04-16 12:14:00 -0400

    Adding readme and another run file.

--- run.php
+++ run.php
@@ -1,7 +1,7 @@
 <?php
 require_once('tcx.php');
 $tcx = new Wappr\tcx;
-$tcx->filename = 'run.tcx';
+$tcx->filename = 'runs/tcx/4-16-16.tcx';
 $tcx->readFile();
 $tcx->loadTcx();
 echo trim($tcx->tcx->Activities->Activity->Id) . "\r\n";
@@ -9,5 +9,5 @@ echo 'Start time: ' . $tcx->tcx->Activities->Activity->Lap['StartTime'] . "\r\n"
 echo 'Total time: ' . $tcx->getTotalTime() . "\r\n";
 echo 'Total minutes: ' . $tcx->getTotalMinutes() . "\r\n";
 $tcx->createKml();
-$tcx->writeKml();
+$tcx->writeKml('runs/kml/4-16-16.kml');
 #var_dump($tcx->kml);
